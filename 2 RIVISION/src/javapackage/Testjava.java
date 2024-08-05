a8117ee072c9f07c1292effdca4003504a70e72f
msher <msher@LAPTOP-I42TTBGV.mynetworksettings.com>
2024-08-05 12:27:37 -0400

    date

---
+++ 2 RIVISION/src/javapackage/Testjava.java
@@ -0,0 +1,10 @@
+package javapackage;
+
+public class Testjava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("rafth coding is correct");
+	}
+
+}
